228b1edd1eff225720a462aaf007c33e9c233e63
Kaila Burgin <kailaburgin@gmail.com>
2024-02-26 21:20:10 -0500

    completed steps 17, 18, and 19

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,61 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./SearchResults.module.css";
 
-// const tracks = [
-//   {
-//     trackName: "Track1",
-//     trackArtist: "Artist1",
-//     trackAlbum: "Album1",
-//     trackID: "id1",
-//   },
-//   {
-//     trackName: "Track2",
-//     trackArtist: "Artist2",
-//     trackAlbum: "Album2",
-//     trackID: "id2",
-//   },
-//   {
-//     trackName: "Track3",
-//     trackArtist: "Artist3",
-//     songAtrackAlbumlbum: "Album3",
-//     trackID: "id3",
-//   },
-// ];
-
-// const Track = ({ trackName, trackArtist, trackAlbum, trackID }) => (
-//   <div style={{ border: "1px solid black", marginRight: "2em" }}>
-//     <li key={trackID} style={{ listStyle: "none" }}>
-//       <h2>{trackName}</h2>
-//       <h3>
-//         {trackArtist},{trackAlbum}
-//       </h3>
-//     </li>
-//     <button>Add to playlist</button>
-//   </div>
-// );
-
-// const Tracklist = tracks.map(Track);
-
-export default function SearchResults() {
+function SearchResults() {
   return (
-    <div
-      className="SearchResults-header"
-      style={{
-        border: "1px solid black",
-        marginRight: "2em",
-        marginLeft: "2em",
-      }}
-    >
-      <h1>Results</h1>
-      <div
-        style={{
-          border: "1px solid black",
-          marginRight: "2em",
-          marginLeft: "2em",
-        }}
-      >
-        <ul>{}</ul>
-      </div>
+    <div className={styles.SearchResults}>
+      {/* <!-- Add a TrackList component --> */}
     </div>
   );
 }
+
+export default SearchResults;
