9f962c94147a1e98325dbef7f2b4456597743c43
Kaila Burgin <kailaburgin@gmail.com>
2024-02-26 20:57:30 -0500

    completed step 11

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,19 @@
-import "./App.css";
-import SearchBar from "./SearchBar";
-import SearchResults from "./SearchResults"
-import Playlist from "./Playlist"
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SearchBar />
-      </header>
-      <body className="App-body">
-        <SearchResults />
-        <Playlist />
-      </body>
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        {/* <!-- Add a SearchBar component --> */}
+
+        <div className="App-playlist">
+          {/* <!-- Add a SearchResults component --> */}
+          {/* <!-- Add a Playlist component --> */}
+        </div>
+      </div>
     </div>
   );
 }
