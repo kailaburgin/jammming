4826159c8fdce89a9aad20a1f9dcd860ca424222
Kaila Burgin <kailaburgin@gmail.com>
2024-02-26 21:23:29 -0500

    completed steps 20, 21, 22

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,31 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Playlist.module.css";
 
-export default function Playlist() {
-  const [playList, setPlaylist] = useState([]);
-
+function Playlist() {
   return (
-    <div
-      style={{
-        border: "1px solid black",
-        marginLeft: "2em",
-      }}
-    >
-      <input
-        type="playlist-name"
-        placeholder="Playlist Name..."
-        className={styles.inputStyle}
-      />
-      <p
-        style={{
-          border: "1px solid black",
-          marginRight: "2em",
-          marginLeft: "2em",
-        }}
-      >
-        Track
-      </p>
-      <button>Add to Spotify</button>
+    <div className={styles.Playlist}>
+      <input defaultValue={"New Playlist"} />
+      {/* <!-- Add a TrackList component --> */}
+      <button className={styles["Playlist-save"]}>SAVE TO SPOTIFY</button>
     </div>
   );
 }
+
+export default Playlist;
